cc47beef3ca8c47c0b03064bde89c6af83ae7024
Velox <mohabbat.bd2020@gmail.com>
2024-03-19 19:23:45 +0600

    Update Problem6.cpp

--- WEEK 1/DAY 5/Problem6.cpp
+++ WEEK 1/DAY 5/Problem6.cpp
@@ -1,47 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+// https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+vector<long long> printFirstNegativeInteger(long long int A[],
+        long long int N, long long int K) {
+            
+        long long i=0,j=0;
 
-vector<long long> printFirstNegativeInteger(long long int v[], long long int len, long long int k);
-
-// Driver program to test above functions
-int main() {
-    long long int t, i;
-    cin >> t;
-    while (t--) {
-        long long int n;
-        cin >> n;
-        long long int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        long long int k;
-        cin >> k;
-
-        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
-        for (auto it : ans) cout << it << " ";
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
-
-// #define ll long long
-vector<long long> printFirstNegativeInteger(long long int v[], long long int len, long long int k) {
-    long long i=0,j=0;
-
-    vector<long long> vv;
-    queue<int> qu;
-    while(j<len){
-
-        if(v[j]<0){
-            qu.push(v[j]);
-        }
-        if(j-i+1==k){
+        vector<long long> vv;
+        queue<int> qu;
+        
+        while(j<N){
+             if(A[j]<0){
+                qu.push(A[j]);
+            }
+            
+            if(j-i+1==K){
             if(qu.empty()) vv.push_back(0);
             else vv.push_back(qu.front());
-            if(!qu.empty() && v[i]==qu.front()){
+            if(!qu.empty() && A[i]==qu.front()){
                 // cout << endl <<  "ef" << v[i] << " "  << qu.front() << endl;
                 qu.pop();
                 // cout <<"fro: " << qu.front() << endl;
@@ -50,9 +24,11 @@ vector<long long> printFirstNegativeInteger(long long int v[], long long int len
             i++;
             j++;
 
+            }
+            
+            else j++;
         }
-
-        else j++;
-    }
-    return vv;
+        
+        return vv;
+                                                                     
  }
