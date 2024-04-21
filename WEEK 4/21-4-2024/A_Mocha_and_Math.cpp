b97b42f9fea38eb968d760b4e2f05c46e01bd2f2
Velox <mohabbat.bd2020@gmail.com>
2024-04-22 00:12:12 +0600

    Add files via upload

---
+++ WEEK 4/21-4-2024/A_Mocha_and_Math.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll len; cin >> len;
+
+    vector<ll> v(len);
+    
+    ll mn=INT_MAX;
+    for(int x=0; x<len; x++){
+        cin >> v[x];
+        mn=min(mn,v[x]);
+    }
+
+    ll ans = mn;
+    for(int x=0; x<len; x++){
+        ans &=v[x];
+    }
+
+    cout << ans << endl;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
---
+++ WEEK 4/21-4-2024/A_XORwice.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll a,b; cin >> a >> b;
+
+    cout << (a^b) << endl;
+
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
