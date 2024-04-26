857af53c0a1c3059ba2f144545aaedf5ffd54ff5
Velox <mohabbat.bd2020@gmail.com>
2024-04-26 23:08:13 +0600

    Add files via upload

---
+++ WEEK 5/DAY 8/A_Payment_Without_Change.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll a,b,c,s;
+    cin >> a >> b >> c >> s;
+
+    ll get=c*a+b;
+    if(c*a>s) get=((s/c)*c)+b;
+    if(get>=s) YES;
+    else NO;
+
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
