23cc65110cce9c36902639071840535c95ea4ee2
Velox <mohabbat.bd2020@gmail.com>
2024-05-10 23:46:28 +0600

    Add files via upload

---
+++ WEEK 6/DAY 15/Factory_Machines.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll len, need; cin >> len >> need;
+    vector<ll> v(len);
+    for(int x=0; x<len; x++) cin >> v[x];
+
+    srt(v);
+
+    auto ok = [&](ll mid){
+
+        ll cnt=0;
+        for(int x=0; x<len; x++){
+            cnt+=(mid/v[x]);
+            if(cnt>=need) return true; 
+        }
+
+        return false;
+    };
+    
+
+    ll l=0,r=1e18, ans=0;
+
+    while(l<=r){
+        ll mid = l+(r-l)/2;
+
+        if(ok(mid)){
+            ans=mid;
+            r=mid-1;
+        }
+
+        else l = mid+1;
+    }
+
+    cout <<  ans << endl;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t=1;// cin >> t;
+    while(t--) solve();
+    return 0;
+}
