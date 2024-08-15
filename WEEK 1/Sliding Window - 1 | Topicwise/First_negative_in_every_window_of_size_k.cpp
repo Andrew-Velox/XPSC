c1c1c201f741c9d961ace71202bfbc88b66e8391
Velox <mohabbat.bd2020@gmail.com>
2024-08-15 10:00:54 +0600

    Create First_negative_in_every_window_of_size_k.cpp

---
+++ WEEK 1/Sliding Window - 1 | Topicwise/First_negative_in_every_window_of_size_k.cpp
@@ -0,0 +1,35 @@
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+        long long int N, long long int K) {
+            
+        long long i=0,j=0;
+
+        vector<long long> vv;
+        queue<int> qu;
+        
+        while(j<N){
+             if(A[j]<0){
+                qu.push(A[j]);
+            }
+            
+            if(j-i+1==K){
+            if(qu.empty()) vv.push_back(0);
+            else vv.push_back(qu.front());
+            if(!qu.empty() && A[i]==qu.front()){
+                // cout << endl <<  "ef" << v[i] << " "  << qu.front() << endl;
+                qu.pop();
+                // cout <<"fro: " << qu.front() << endl;
+
+            } 
+            i++;
+            j++;
+
+            }
+            
+            else j++;
+        }
+        
+        return vv;
+                                                                     
+ }
